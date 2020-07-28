2c8dc5b2e2483859201e8d8a0c07b3f3e211c282
jasonwynn10 <jasonwynn10@me.com>
2020-07-27 21:18:37 -0400

    Optimize if checks

--- src/jasonwynn10/NativeDimensions/block/Fire.php
+++ src/jasonwynn10/NativeDimensions/block/Fire.php
@@ -12,101 +12,58 @@ class Fire extends PMFire {
 
 	public function onNearbyBlockChange() : void {
 		foreach($this->getAllSides() as $side => $block) {
-			if($block instanceof \pocketmine\block\Obsidian) {
-				$minWidth = 2;
-				if($this->testDirectionForObsidian(Vector3::SIDE_NORTH, $this, $width)) {
-					$totalWidth = $width;
-					if($this->testDirectionForObsidian(Vector3::SIDE_SOUTH, $this, $width)) {
-						$totalWidth += $width-1;
-						if($totalWidth < $minWidth) {
-							parent::onNearbyBlockChange();
-							return;
-							// portal cannot be made
-						}
-						$direction = Vector3::SIDE_NORTH;
-						// found portal width
-					}elseif($this->testDirectionForObsidian(Vector3::SIDE_EAST, $this, $width)) {
-						$totalWidth = $width;
-						if($this->testDirectionForObsidian(Vector3::SIDE_WEST, $this, $width)) {
-							$totalWidth += $width-1;
-							if($totalWidth < $minWidth) {
-								parent::onNearbyBlockChange();
-								return;
-								// portal cannot be made
-							}
-							$direction = Vector3::SIDE_EAST;
-							// found portal width
-						}else{
-							parent::onNearbyBlockChange();
-							return;
-							// portal cannot be made
-						}
-						$direction = Vector3::SIDE_EAST;
-						// found portal width
-					}else{
-						parent::onNearbyBlockChange();
-						return;
-						// portal cannot be made
-					}
-				}elseif($this->testDirectionForObsidian(Vector3::SIDE_EAST, $this, $width)) {
-					$totalWidth = $width;
-					if($this->testDirectionForObsidian(Vector3::SIDE_WEST, $this, $width)) {
-						$totalWidth += $width-1;
-						if($totalWidth < $minWidth) {
-							parent::onNearbyBlockChange();
-							return;
-							// portal cannot be made
-						}
-						$direction = Vector3::SIDE_EAST;
-						// found portal width
-					}
-					$direction = Vector3::SIDE_EAST;
-					// found portal width
-				}else{
-					parent::onNearbyBlockChange();
-					return;
-					// portal cannot be made
-				}
-
-				$minHeight = 3;
-				if($this->testDirectionForObsidian(Vector3::SIDE_UP, $this, $height)) {
-					$totalHeight = $height;
-					if($this->testDirectionForObsidian(Vector3::SIDE_DOWN, $this, $height)) {
-						$totalHeight += $height-1;
-						if($totalHeight < $minHeight) {
-							parent::onNearbyBlockChange();
-							return;
-							// portal cannot be made
-						}
-						// found portal height
-					}else{
-						parent::onNearbyBlockChange();
-						return;
-						// portal cannot be made
-					}
-					// found portal height
-				}else{
-					parent::onNearbyBlockChange();
-					return;
-					// portal cannot be made
-				}
-
-				$this->testDirectionForObsidian($direction, $this, $horizblocks);
-				$start = $this->asPosition()->getSide($direction, $horizblocks-1);
-				$this->testDirectionForObsidian(Vector3::SIDE_UP, $this, $vertblocks);
-				$start = Position::fromObject($start->add(0, $vertblocks-1, 0), $start->getLevelNonNull());
-
-				for($j = 0; $j < $totalHeight; ++$j) {
-					for($k = 0; $k < $totalWidth; ++$k) {
-						if($direction == Vector3::SIDE_NORTH) {
-							$start->getLevelNonNull()->setBlock($start->add(0,-$j,$k), BlockFactory::get(BlockIds::PORTAL), false, false);
-						}else{
-							$start->getLevelNonNull()->setBlock($start->add(-$k,-$j), BlockFactory::get(BlockIds::PORTAL), false, false);
-						}
-					}
-				}
-				return;
+			if(!$block instanceof \pocketmine\block\Obsidian) {
+				continue;
 			}
+			$minWidth = 2;
+			if($this->testDirectionForObsidian(Vector3::SIDE_NORTH, $this, $widthA) and $this->testDirectionForObsidian(Vector3::SIDE_SOUTH, $this, $widthB)) {
+				$totalWidth = $widthA + $widthB - 1;
+				if($totalWidth < $minWidth) {
+					parent::onNearbyBlockChange();
+					return; // portal cannot be made
+				}
+				$direction = Vector3::SIDE_NORTH;
+			}elseif($this->testDirectionForObsidian(Vector3::SIDE_EAST, $this, $widthA) and $this->testDirectionForObsidian(Vector3::SIDE_WEST, $this, $widthB)) {
+				$totalWidth = $widthA + $widthB - 1;
+				if($totalWidth < $minWidth) {
+					parent::onNearbyBlockChange();
+					return; // portal cannot be made
+				}
+				$direction = Vector3::SIDE_EAST;
+				// found portal width
+			}else{
+				parent::onNearbyBlockChange();
+				return; // portal cannot be made
+			}
+
+			$minHeight = 3;
+			if($this->testDirectionForObsidian(Vector3::SIDE_UP, $this, $heightA) and $this->testDirectionForObsidian(Vector3::SIDE_DOWN, $this, $heightB)) {
+				$totalHeight = $heightA + $heightB - 1;
+				if($totalHeight < $minHeight) {
+					parent::onNearbyBlockChange();
+					return; // portal cannot be made
+				}
+				// found portal height
+			}else{
+				parent::onNearbyBlockChange();
+				return; // portal cannot be made
+			}
+
+			$this->testDirectionForObsidian($direction, $this, $horizblocks);
+			$start = $this->asPosition()->getSide($direction, $horizblocks-1);
+			$this->testDirectionForObsidian(Vector3::SIDE_UP, $this, $vertblocks);
+			$start = Position::fromObject($start->add(0, $vertblocks-1, 0), $start->getLevelNonNull());
+
+			for($j = 0; $j < $totalHeight; ++$j) {
+				for($k = 0; $k < $totalWidth; ++$k) {
+					if($direction == Vector3::SIDE_NORTH) {
+						$start->getLevelNonNull()->setBlock($start->add(0, -$j, $k), BlockFactory::get(BlockIds::PORTAL), false, false);
+					}else{
+						$start->getLevelNonNull()->setBlock($start->add(-$k, -$j), BlockFactory::get(BlockIds::PORTAL), false, false);
+					}
+				}
+			}
+			return;
 		}
 		parent::onNearbyBlockChange();
 	}
