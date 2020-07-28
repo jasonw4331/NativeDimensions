e437d4bce99caa0dd9793788c3cef68ef34d79a7
jasonwynn10 <jasonwynn10@me.com>
2020-07-27 23:22:17 -0400

    Portals can break themselves, use pocketmine to initiate

--- src/jasonwynn10/NativeDimensions/block/Obsidian.php
+++ src/jasonwynn10/NativeDimensions/block/Obsidian.php
@@ -35,84 +35,17 @@ declare(strict_types=1);
 
 namespace jasonwynn10\NativeDimensions\block;
 
-use pocketmine\block\Air;
-use pocketmine\block\Block;
 use pocketmine\block\Obsidian as PMObsidian;
-use pocketmine\item\Item;
-use pocketmine\math\Vector3;
-use pocketmine\Player;
 
 class Obsidian extends PMObsidian {
 
-	public function onBreak(Item $item, Player $player = null): bool {
-		parent::onBreak($item);
-		foreach($this->getAllSides() as $i => $block){
-			if($block instanceof Portal){
-				if($block->getSide(Vector3::SIDE_WEST) instanceof Portal or
-					$block->getSide(Vector3::SIDE_EAST) instanceof Portal
-				) {//x方向
-					for(
-						$x = $block->x; $this->getLevel()->getBlockIdAt($x, $block->y, $block->z) == Block::PORTAL; $x++
-					) {
-						for(
-							$y = $block->y; $this->getLevel()->getBlockIdAt($x, $y, $block->z) == Block::PORTAL; $y++
-						) {
-							$this->getLevel()->setBlock(new Vector3($x, $y, $block->z), new Air());
-						}
-						for(
-							$y = $block->y - 1; $this->getLevel()->getBlockIdAt($x, $y, $block->z) == Block::PORTAL; $y--
-						) {
-							$this->getLevel()->setBlock(new Vector3($x, $y, $block->z), new Air());
-						}
-					}
-					for(
-						$x = $block->x - 1; $this->getLevel()->getBlockIdAt($x, $block->y, $block->z) == Block::PORTAL; $x--
-					) {
-						for(
-							$y = $block->y; $this->getLevel()->getBlockIdAt($x, $y, $block->z) == Block::PORTAL; $y++
-						) {
-							$this->getLevel()->setBlock(new Vector3($x, $y, $block->z), new Air());
-						}
-						for(
-							$y = $block->y - 1; $this->getLevel()->getBlockIdAt($x, $y, $block->z) == Block::PORTAL; $y--
-						) {
-							$this->getLevel()->setBlock(new Vector3($x, $y, $block->z), new Air());
-						}
-					}
-				} else {//z方向
-					for(
-						$z = $block->z; $this->getLevel()->getBlockIdAt($block->x, $block->y, $z) == Block::PORTAL; $z++
-					) {
-						for(
-							$y = $block->y; $this->getLevel()->getBlockIdAt($block->x, $y, $z) == Block::PORTAL; $y++
-						) {
-							$this->getLevel()->setBlock(new Vector3($block->x, $y, $z), new Air());
-						}
-						for(
-							$y = $block->y - 1; $this->getLevel()->getBlockIdAt($block->x, $y, $z) == Block::PORTAL; $y--
-						) {
-							$this->getLevel()->setBlock(new Vector3($block->x, $y, $z), new Air());
-						}
-					}
-					for(
-						$z = $block->z - 1;$this->getLevel()->getBlockIdAt($block->x, $block->y, $z) == Block::PORTAL; $z--
-					) {
-						for(
-							$y = $block->y; $this->getLevel()->getBlockIdAt($block->x, $y, $z) == Block::PORTAL; $y++
-						) {
-							$this->getLevel()->setBlock(new Vector3($block->x, $y, $z), new Air());
-						}
-						for(
-							$y = $block->y - 1; $this->getLevel()->getBlockIdAt($block->x, $y, $z) == Block::PORTAL; $y--
-						) {
-							$this->getLevel()->setBlock(new Vector3($block->x, $y, $z), new Air());
-						}
-					}
-				}
-				return true;
-			}
+	public function getAffectedBlocks() : array {
+		$return = [$this];
+		foreach($this->getAllSides() as $side => $block) {
+			if($block instanceof Portal)
+				$return[] = $block;
 		}
-
-		return true;
+		return $return;
 	}
+
 }
