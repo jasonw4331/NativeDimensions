a4c6f3b46bdf82186cf053e165fe16b22e171d72
jasonwynn10 <jasonwynn10@me.com>
2022-01-04 16:33:44 -0500

    Added TODO APIs

---
+++ src/world/DimensionalWorld.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+namespace jasonwynn10\NativeDimensions\world;
+
+use pocketmine\world\World;
+
+class DimensionalWorld extends World {
+	public function getOverworld() : DimensionalWorld {
+		// TODO
+	}
+
+	public function getNether() : DimensionalWorld {
+		// TODO
+	}
+
+	public function getEnd() : DimensionalWorld {
+		// TODO
+	}
+}
