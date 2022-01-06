3df566bbaf397197391c46a3ca1d19abd2266e02
jasonwynn10 <jasonwynn10@me.com>
2022-01-06 03:15:44 -0500

    Implemented end portal teleport

--- src/block/EndPortal.php
+++ src/block/EndPortal.php
@@ -2,6 +2,8 @@
 declare(strict_types=1);
 namespace jasonwynn10\NativeDimensions\block;
 
+use jasonwynn10\NativeDimensions\Main;
+use jasonwynn10\NativeDimensions\world\DimensionalWorld;
 use pocketmine\block\BlockBreakInfo;
 use pocketmine\block\BlockIdentifier as BID;
 use pocketmine\block\BlockLegacyIds as Ids;
@@ -10,8 +12,12 @@ use pocketmine\block\Opaque;
 use pocketmine\block\utils\BlockDataSerializer;
 use pocketmine\block\utils\FacesOppositePlacingPlayerTrait;
 use pocketmine\block\utils\HorizontalFacingTrait;
+use pocketmine\entity\Entity;
 use pocketmine\math\AxisAlignedBB;
 use pocketmine\math\Facing;
+use pocketmine\player\Player;
+use pocketmine\scheduler\ClosureTask;
+use pocketmine\world\Position;
 
 class EndPortal extends Opaque{
 	use FacesOppositePlacingPlayerTrait;
@@ -46,4 +52,31 @@ class EndPortal extends Opaque{
 	protected function recalculateCollisionBoxes() : array{
 		return [AxisAlignedBB::one()->trim(Facing::UP, 5 / 16)];
 	}
+
+	public function hasEntityCollision() : bool{
+		return true;
+	}
+
+	public function onEntityInside(Entity $entity): bool{
+		/** @var DimensionalWorld $world */
+		$world = $entity->getPosition()->getWorld();
+		if($world->getEnd() === $world)
+			return true;
+
+		if(!in_array($entity->getId(), Main::getTeleporting())){
+			Main::addTeleportingId($entity->getId());
+
+			if($world->getOverworld() === $world) {
+				$position = new Position(100, 50, 0, $world->getEnd());
+				$entity->getWorld()->getLogger()->debug("Teleporting to Nether");
+			}else {
+				$position = $world->getOverworld()->getSafeSpawn();
+				$entity->getWorld()->getLogger()->debug("Teleporting to Overworld");
+			}
+
+			$entity->teleport($position);
+		}
+		return true;
+	}
+
 }
