5284dfb6b3ddf60796dae0e44c1eb7c9e15a239f
jasonwynn10 <jasonwynn10@me.com>
2022-01-05 23:46:23 -0500

    Work on teleporting logic a bit

--- src/block/Portal.php
+++ src/block/Portal.php
@@ -18,6 +18,8 @@ use pocketmine\math\Facing;
 use pocketmine\math\Vector3;
 use pocketmine\player\Player;
 use pocketmine\scheduler\ClosureTask;
+use pocketmine\world\ChunkLoader;
+use pocketmine\world\format\Chunk;
 use pocketmine\world\Position;
 use pocketmine\world\World;
 
@@ -74,65 +76,122 @@ class Portal extends NetherPortal {
 		// TODO: levelDB portal mapping
 	}
 
-	public function onEntityCollide(Entity $entity): void{
-		/** @var DimensionalWorld $world */
-		$world = $entity->getPosition()->getWorld();
-		$position = $this->getPair();
-		if($position === null) {
-			$position = $this->getPosition();
-			if($world->getOverworld() === $world) {
-				$world = $world->getNether();
-				$x = $position->x * 8;
-				$z = $position->z * 8;
-			}else {
-				$world = $world->getOverworld();
-				$x = $position->x / 8;
-				$z = $position->z / 8;
-			}
-			$y = $position->y;
-			$validBlock = $this->getGenerationSpace($x, $z, $world);
-			if($validBlock instanceof Position) {
-				$this->makePortal($validBlock);
-			}else {
-				$this->makePortal(new Position($x, $y, $z, $world));
-			}
-		}elseif(!in_array($entity->getId(), Main::getTeleporting())){
-			if($entity instanceof Player) {
-				Main::addTeleportingId($entity->getId());
-				if($entity->isCreative()) {
-					$entity->teleport($position);
-					return;
+	public function onEntityInside(Entity $entity): bool{
+		if(!in_array($entity->getId(), Main::getTeleporting())){
+			Main::addTeleportingId($entity->getId());
+			$entity->getWorld()->getLogger()->debug("Portal Teleport initialised");
+			/** @var DimensionalWorld $world */
+			$world = $entity->getPosition()->getWorld();
+			$position = $this->getPair();
+			if($position === null) {
+				$position = $this->getPosition();
+				if($world->getOverworld() === $world) {
+					$world = $world->getNether();
+					$x = $position->x * 8;
+					$z = $position->z * 8;
+				}else {
+					$world = $world->getOverworld();
+					$x = $position->x / 8;
+					$z = $position->z / 8;
 				}
-				Main::getInstance()->getScheduler()->scheduleDelayedTask(new ClosureTask(function() use($entity, $position) : void {
-					if(!$entity->getPosition()->getWorld()->getBlock($entity->getPosition()->floor()) instanceof NetherPortal) {
-						return;
-					}
-					$entity->teleport($position);
-				}), 20 * 6);
-			}else{
-				$entity->teleport($position);
-			}
-		}
-	}
+				$y = $position->y;
 
-	public function getGenerationSpace(float $x, float $z, World $world) : ?Position {
-		for($chunkX = ($x >> 4) - 3; $chunkX <= ($x >> 4) + 4; ++$chunkX) {
-			for($chunkZ = ($z >> 4) - 3; $chunkZ <= ($z >> 4) + 4; ++$chunkZ) {
-				$chunk = $world->getChunk($chunkX, $chunkZ);
-				for($k = 0; $k < $world->getMaxY(); ++$k) {
-					for($i = 0; $i < 16; ++$i) {
-						for($j = 0; $j < 16; ++$j) {
-							$id = $chunk->getFullBlock($i, $k, $j);
-							$block = BlockFactory::getInstance()->getInstance()->fromFullBlock($id);
-							if($block instanceof Air) {
-								return new Position(($chunkX << 4) + $i, $k, ($chunkZ << 4) + $j, $world);
+				if($world->isChunkGenerated($x >> Chunk::COORD_BIT_SIZE, $z >> Chunk::COORD_BIT_SIZE)) {
+					$world->getLogger()->debug("Generating portal pair");
+					for($chunkX = ($x >> Chunk::COORD_BIT_SIZE) - 3; $chunkX <= ($x >> Chunk::COORD_BIT_SIZE) + 4; ++$chunkX) {
+						for($chunkZ = ($z >> Chunk::COORD_BIT_SIZE) - 3; $chunkZ <= ($z >> Chunk::COORD_BIT_SIZE) + 4; ++$chunkZ) {
+							$validBlock = $this->getGenerationSpace($chunkX, $chunkZ, $world);
+							if($validBlock instanceof Position) {
+								$this->makePortal($validBlock);
+								$position = $validBlock;
+							}else {
+								$this->makePortal($position = new Position($x, $y, $z, $world));
+							}
+							$world->getLogger()->debug("Portal Pair Generated");
+						}
+					}
+
+					if($entity instanceof Player and !$entity->isCreative()) {
+						$world->getLogger()->debug("Registering Teleport Event");
+						Main::getInstance()->getScheduler()->scheduleDelayedTask(new ClosureTask(function() use($entity, $position) : void {
+							if(!$entity->getPosition()->getWorld()->getBlock($entity->getPosition()->floor()) instanceof NetherPortal) {
+								return;
+							}
+							$entity->teleport($position);
+						}), 20 * 6);
+					}else{
+						$world->getLogger()->debug("Teleporting entity");
+						$entity->teleport($position);
+					}
+					return true;
+				}
+				$world->getLogger()->debug("Generating terrain");
+				$portalMade = false;
+				$loader = new class() implements ChunkLoader{};
+				$this->generatePortalSpace($x, $z, $world, $loader, function(Chunk $chunk) use ($x, $y, $z, $world, $entity, $loader, &$portalMade) {
+					$world->getLogger()->debug("Terrain successfully Generated");
+
+					if($portalMade)
+						return;
+
+					$world->getLogger()->debug("Generating portal pair");
+					for($k = 0; $k < $world->getMaxY(); ++$k) {
+						for($i = 0; $i < 16; ++$i) {
+							for($j = 0; $j < 16; ++$j) {
+								$id = $chunk->getFullBlock($i, $k, $j);
+								$block = BlockFactory::getInstance()->getInstance()->fromFullBlock($id);
+								if($block instanceof Air) {
+									$position = new Position(($x >> Chunk::COORD_BIT_SIZE) + $i, $k, ($z >> Chunk::COORD_BIT_SIZE) + $j, $world);
+									Main::getInstance()->getScheduler()->scheduleTask(new ClosureTask(\Closure::fromCallable(function() use($entity, $position) : void {
+										if($entity instanceof Player and !$entity->isCreative()) {
+											$entity->getWorld()->getLogger()->debug("Registering Teleport Event");
+											Main::getInstance()->getScheduler()->scheduleDelayedTask(new ClosureTask(function() use($entity, $position) : void {
+												if(!$entity->getPosition()->getWorld()->getBlock($entity->getPosition()->floor()) instanceof NetherPortal) {
+													return;
+												}
+												$entity->teleport($position);
+											}), 20 * 6);
+										}else{
+											$entity->getWorld()->getLogger()->debug("Teleporting entity");
+											$entity->teleport($position);
+										}
+									})));
+									$this->makePortal($position);
+									$world->getLogger()->debug("Portal Pair Generated");
+									$portalMade = true;
+									break 3;
+								}
 							}
 						}
+					}
+				}, fn() => Main::removeTeleportingId($entity->getId()));
+			}
+		}
+		return true;
+	}
+
+	public function getGenerationSpace(int $chunkX, int $chunkZ, World $world) : ?Position {
+		$chunk = $world->getChunk($chunkX, $chunkZ);
+		for($k = 0; $k < $world->getMaxY(); ++$k) {
+			for($i = 0; $i < 16; ++$i) {
+				for($j = 0; $j < 16; ++$j) {
+					$id = $chunk->getFullBlock($i, $k, $j);
+					$block = BlockFactory::getInstance()->getInstance()->fromFullBlock($id);
+					if($block instanceof Air) {
+						return new Position(($chunkX << Chunk::COORD_BIT_SIZE) + $i, $k, ($chunkZ << Chunk::COORD_BIT_SIZE) + $j, $world);
 					}
 				}
 			}
 		}
 		return null;
+	}
+
+	public function generatePortalSpace(float $x, float $z, World $world, ChunkLoader $loader, callable $onSuccess, callable $onFailure) : void {
+		for($chunkX = ($x >> Chunk::COORD_BIT_SIZE) - 3; $chunkX <= ($x >> Chunk::COORD_BIT_SIZE) + 4; ++$chunkX) {
+			for($chunkZ = ($z >> Chunk::COORD_BIT_SIZE) - 3; $chunkZ <= ($z >> Chunk::COORD_BIT_SIZE) + 4; ++$chunkZ) {
+				$world->orderChunkPopulation($chunkX, $chunkZ, $loader)->onCompletion(\Closure::fromCallable($onSuccess), \Closure::fromCallable($onFailure));
+			}
+		}
 	}
 
 	public function getPair() : ?Position {
@@ -148,16 +207,18 @@ class Portal extends NetherPortal {
 			$z = (int)ceil($this->getPosition()->z / 8);
 		}
 
-		for($chunkX = ($x >> 4) - 3; $chunkX <= ($x >> 4) + 4; ++$chunkX) {
-			for($chunkZ = ($z >> 4) - 3; $chunkZ <= ($z >> 4) + 4; ++$chunkZ) {
-				$chunk = $world->getChunk($chunkX, $chunkZ);
+		for($chunkX = ($x >> Chunk::COORD_BIT_SIZE) - 3; $chunkX <= ($x >> Chunk::COORD_BIT_SIZE) + 4; ++$chunkX) {
+			for($chunkZ = ($z >> Chunk::COORD_BIT_SIZE) - 3; $chunkZ <= ($z >> Chunk::COORD_BIT_SIZE) + 4; ++$chunkZ) {
+				$chunk = $world->loadChunk($chunkX, $chunkZ);
+				if($chunk === null)
+					return null;
 				for($k = $world->getMinY(); $k < $world->getMaxY(); ++$k) {
 					for($i = 0; $i < 16; ++$i) {
 						for($j = 0; $j < 16; ++$j) {
 							$id = $chunk->getFullBlock($i, $k, $j);
 							$block = BlockFactory::getInstance()->getInstance()->fromFullBlock($id);
 							if($block instanceof Portal and BlockFactory::getInstance()->getInstance()->fromFullBlock($chunk->getFullBlock($i, $k-1, $j)) instanceof Portal) {
-								return new Position(($chunkX << 4) + $i, $k, ($chunkZ << 4) + $j, $world);
+								return new Position(($chunkX << Chunk::COORD_BIT_SIZE) + $i, $k, ($chunkZ << Chunk::COORD_BIT_SIZE) + $j, $world);
 							}
 						}
 					}
@@ -176,7 +237,7 @@ class Portal extends NetherPortal {
 			return false; // no portals in the end
 		$xDirection = (bool)mt_rand(0,1);
 		if($xDirection) {
-			// portals
+			// portal blocks
 			$world->setBlock($position, (clone $this)->setAxis(Axis::Z), true);
 			$world->setBlock($position->getSide(Facing::UP), (clone $this)->setAxis(Axis::Z), true);
 			$world->setBlock($position->getSide(Facing::UP, 2), (clone $this)->setAxis(Axis::Z), true);
@@ -212,7 +273,7 @@ class Portal extends NetherPortal {
 			$world->setBlock($position->getSide(Facing::EAST)->getSide(Facing::UP)->getSide(Facing::WEST), VanillaBlocks::AIR(), true);
 			$world->setBlock($position->getSide(Facing::EAST)->getSide(Facing::UP, 2)->getSide(Facing::WEST), VanillaBlocks::AIR(), true);
 		}else{
-			// portals
+			// portal blocks
 			$world->setBlock($position, (clone $this)->setAxis(Axis::X), true);
 			$world->setBlock($position->getSide(Facing::UP), (clone $this)->setAxis(Axis::X), true);
 			$world->setBlock($position->getSide(Facing::UP, 2), (clone $this)->setAxis(Axis::X), true);
